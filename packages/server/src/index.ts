63fa82ff0b6ab151d74434dd3d6ee2204e84044b
Baatchia Riyadh <Baatchia_Riyadh@protonmail.com>
2021-11-14 13:17:16 +0100

    feat(server): serve client build from server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import path from 'path';
 const app = express();
 const port = 4000;
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
 	res.send('Hello World!');
+});
+
+app.use(express.static(path.join(__dirname, '../../client/build')));
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
 });
 
 app.listen(port, () => {
